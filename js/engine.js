3c058595b9234369a8a3e603d1ad0463dfb1f571
Romklao <rk120879@gmail.com>
2018-04-18 14:23:42 -0700

    Update comment

--- js/engine.js
+++ js/engine.js
@@ -84,7 +84,7 @@ var Engine = (function(global) {
     }
 
     /* This is called by the update function and loops through all of the
-     * objects within your allEnemies array as defined in app.js and calls
+     * objects within your enemies array as defined in app.js and calls
      * their update() methods. It will then call the update function for your
      * player object. These update methods should focus purely on updating
      * the data/properties related to the object. Do your drawing in your
