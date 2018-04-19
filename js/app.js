4fccb7b0e9fda3f6500406997f1d547d8c03fabb
Romklao <rk120879@gmail.com>
2018-04-18 18:46:44 -0700

    Add comment in app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 'use strict';
+/* Romklao Chainuwong rchai2020@gmail.com
+   This project was built in April 15, 2018 */
 
-/* This file includes objects such as player, enemies, selectPlayer etc.
+/*   This file includes objects such as player, enemies, selectPlayer etc.
    to update their appearance in the view. It will be used in engine.js file
 */
 
@@ -87,7 +89,7 @@ var Heart = function(x, y, originalPosition, width, height) {
     Item.call(this, x, y, originalPosition, width, height);
     this.sprite = 'images/Heart.png';
 }
-
+// Inherit all methods from Item class
 Heart.prototype = Object.create(Item.prototype);
 
 var hearts = [];
@@ -105,6 +107,7 @@ var Gem = function(x, y, originalPosition, width, height) {
     this.sprite = 'images/Gem Blue.png';
 };
 
+// Inherit all methods from Item class
 Gem.prototype = Object.create(Item.prototype);
 
 var gems = [];
@@ -126,6 +129,7 @@ var Rock = function(x, y, originalPosition, width, height) {
     this.height = 130;
 }
 
+// Inherit all methods from Item class
 Rock.prototype = Object.create(Item.prototype);
 
 var rocks = [];
@@ -142,7 +146,7 @@ var rock9 = new Rock(210, 175);
 
 rocks.push(rock1, rock2, rock3, rock4, rock5, rock6, rock7, rock8, rock9);
 
-// Create Enemy class our player must avoid
+// Create Enemy class to set original position all enemies. A player must avoid enemies.
 var Enemy = function(x, y, originalPosition) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
@@ -162,9 +166,10 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += this.speed * dt;
-
+    /* This will teleport bugs to the start position
+    when they reach the right endpoint of canvas width */
     if (this.x > 707) {
-        this.x = -70;//Bugs teleport to starting point
+        this.x = -70;//Bugs move to starting point
     }
 };
 
@@ -174,7 +179,10 @@ Enemy.prototype.render = function() {
 };
 
 /************** PlayGame class ***************/
-/************** Start, Reset, Win, Lose *************/
+/* This sets sound effect for select a charactor, gain life, get gem and dead.
+   Provide alert on the screen when player win and lose.
+   Reset the screen after a player lose.
+*/
 var PlayGame = function() {
     this.gainLifeSound = new Audio('audio/jingle-achievement.wav');
     this.getGemSound = new Audio('audio/collect-point.wav');
@@ -182,31 +190,38 @@ var PlayGame = function() {
     this.select = new Audio('audio/pickup.wav');
 }
 
+// Alert when a player loses and reset the screen to start play game again
 PlayGame.prototype.lose = function() {
     alert(`GAME OVER! Your score is ${player.score}.`);
     this.resetGame();
 };
 
+// Alert when a player wins the game and reset to play again
 PlayGame.prototype.win = function() {
     alert(`You are the winner! Your score is ${player.score}.`);
     this.resetGame();
 };
 
-// Reset the game when game over
+// Reset the game when game over or win
 PlayGame.prototype.resetGame = function() {
+    // Set a player to original position
     player.reset();
 
+    // Empty collectHearts and collectedGem array
     collectedHearts = [];
     collectedGems = [];
 
+    // Set hearts to original position
     hearts.forEach(function(heart) {
         heart.reset();
     });
 
+    // Set gems to original position
     gems.forEach(function(gem) {
         gem.reset();
     });
 
+    // Set rocks to original position
     rocks.forEach(function(rock) {
         rock.reset();
     })
@@ -214,11 +229,12 @@ PlayGame.prototype.resetGame = function() {
 
 var playGame = new PlayGame();
 
-// Player class requires an update(), render() and
-// a handleInput() method. This will track score, lives, dead
+/* Create Player class to settle a player's position, width, height, lives and score
+   Player class requires an update(), render() and
+   a handleInput() method. This will track score, lives, dead */
 var Player = function() {
     Item.call(this, 304, 420);
-    this.playerPosition = [];
+    this.playerPosition = []; // Record x,y coordinates of the player for non-enemy collision
     this.width = 100;
     this.height = 150;
     this.speed = 50;
@@ -230,6 +246,9 @@ var Player = function() {
     this.gainLife = false;
 }
 
+/* When a player wins or loses the game, reset the player
+   lives and score to original value
+*/
 Player.prototype.reset = function() {
     this.x = 304;
     this.y = 420;
@@ -241,14 +260,18 @@ Player.prototype.reset = function() {
     document.getElementById('score').innerHTML = this.score.toString();
 }
 
-// When a player collides with enemy
+/* This method shows the event when a player collides with an enemy
+   if lives > 1, they will decrease by -1
+*/
 Player.prototype.collide = function() {
     if (this.lives === 1) {
         this.lives -= 1;
         this.score -= 2000;
+
+        //Keep record of lives and score when a player hits an enemy
         document.getElementById('lives').innerHTML = this.lives.toString();
         document.getElementById('score').innerHTML = this.score.toString();
-
+        // Game over when no life left
         setTimeout(function() {
             playGame.lose();
         }, 800);
@@ -256,11 +279,12 @@ Player.prototype.collide = function() {
     } else if (this.lives > 1) {
         this.lives -= 1;
         this.score -= 2000;
+        //Keep record of lives and score when a player hits an enemy
         document.getElementById('lives').innerHTML = this.lives.toString();
         document.getElementById('score').innerHTML = this.score.toString();
 
         if (this.score < 0) {
-            // Set timeout for alert to popup after updating lives
+            // Set timeout for alert of losing game after updating lives and score
             setTimeout(function() {
                 playGame.lose();
             }, 800);
@@ -268,7 +292,7 @@ Player.prototype.collide = function() {
     }
 };
 
-// When hit a rock, a player cannot move pass rocks
+// FaceRock method gives how far between a rock and a player that it cannot move pass a rock
 Player.prototype.faceRock = function() {
     for(var i = 0; i < rocks.length; i++) {
         if (this.x < rocks[i].x + 50 &&
@@ -276,32 +300,33 @@ Player.prototype.faceRock = function() {
             this.y < rocks[i].y + 50 &&
             this.y + 50 > rocks[i].y) {
 
+            // Player position stay the same when face a rock
             this.x = this.playerPosition[this.playerPosition.length-1][0];
             this.y = this.playerPosition[this.playerPosition.length-1][1];
         }
     }
 };
 
-// When a player reach water
+// reachWater method: earn 5,000 points when reaching water
 Player.prototype.reachWater = function() {
     if (this.y < 0) {
         playGame.getGemSound.play();
 
         this.score += 5000;
         document.getElementById('score').innerHTML = this.score.toString();
-
+        // Set getGem to true so a diamond appears above a charactor
         this.getGem = true;
         // Set timeout for a diamond to stay longer
         setTimeout(function() {
             player.getGem = false;
         }, 500);
-
+        // Back to start point
         this.x = 304;
         this.y = 420;
     }
 }
 
-// When a player collides with an enemy
+// This will show a cross symbol when a player collides with an enemy
 Player.prototype.checkDead = function() {
     if (!this.dead) {
         for(var i = 0; i < enemies.length; i++) {
@@ -311,9 +336,9 @@ Player.prototype.checkDead = function() {
                 this.y + 50> enemies[i].y) {
 
                 playGame.loseLifeSound.play();
-
+                // Set dead to true so the cross sign appears above a charactor
                 this.dead = true;
-                // Set timeout for a cross to stay longer
+                // Set timeout for the sign to stay longer
                 setTimeout(function() {
                     player.x = 304;
                     player.y = 420;
@@ -326,9 +351,10 @@ Player.prototype.checkDead = function() {
     }
 };
 
-// Collect hearts to gain lives
+// This tracks lives increasing when collect hearts.
 Player.prototype.getHeart = function() {
     for(var i = 0; i < hearts.length; i++) {
+        // Condition of player getting heart
         if (this.x < hearts[i].x &&
             this.x + this.width > hearts[i].x &&
             this.y < hearts[i].y &&
@@ -339,6 +365,7 @@ Player.prototype.getHeart = function() {
             collectedHearts.push([hearts[i].x, hearts[i].y]);
 
             this.lives += 1;
+            // Update lives when collect heart
             document.getElementById('lives').innerHTML = this.lives.toString();
 
             this.gainLife = true;
@@ -353,7 +380,7 @@ Player.prototype.getHeart = function() {
     }
 };
 
-// Collect gems to increase points
+// This tracks score increasing when collect gems
 Player.prototype.collectGem = function() {
     for (var i = 0; i < gems.length; i++) {
         if (this.x < gems[i].x &&
@@ -379,11 +406,11 @@ Player.prototype.collectGem = function() {
     }
 };
 
-// When a player reaches 30,000 points
+// When a player wins when reache 20,000 points
 Player.prototype.winTheGame = function() {
     if (this.score >= 20000) {
         document.getElementById('score').innerHTML = this.score.toString();
-
+        // Alert when win and reset to restart the game
         setTimeout(function() {
             playGame.win();
         }, 10);
@@ -415,7 +442,7 @@ Player.prototype.update = function() {
     player.winTheGame();
 };
 
-// Render in each condition
+// Render charactor with cross sign, diamond or heart in each condition
 Player.prototype.render = function() {
     if (this.dead) {
         // When dead, show the cross image
